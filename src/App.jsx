906aa6949e934f4fad11f627183cc196ee3710af
rpat9 <rudrap26176@gmail.com>
2025-05-01 15:44:38 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Hero from "./components/Hero"
+
+export default function App() {
+
+  return (
+    <>
+      <div>
+        <Hero />
+      </div>
+    </>
+    
+  )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,56 @@
+import rudraPic from "../assets/rudra-pic.jpg";
+import Typewriter from 'typewriter-effect';
+
+export default function Hero() {
+    return (
+        <section className="min-h-screen flex flex-col-reverse md:flex-row items-center justify-between px-6 md:px-16 py-12 fade-in">
+
+            <div className="flex-1 text-center md:text-left">
+
+                <h1 className="text-4xl md:text-5xl font-bold mb-4 text-[var(--color-primary)]">
+                    Hi, I'm {' '}
+                    <span className="inline-block">
+
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter
+                                .typeString('Rudra Patel')
+                                .callFunction(() => {
+                                    const cursor = document.querySelector('.Typewriter__cursor');
+                                    if (cursor) cursor.style.display = 'none';
+                                })
+                                .start();
+                            }}
+
+                            options={{
+                                autoStart: true,
+                                delay: 300,
+                                loop: false,
+                            }}
+                            />
+
+                    </span>
+                </h1>
+
+                <p className="text-lg md:text-xl mb-6 text-[var(--color-text-secondary)]">
+                    Aspiring software engineer with hands-on experience building web applications in JavaScript, React, Node.js, and Python. Skilled in Agile development, cloud deployment, and creating scalable, user-focused solutions. Strong collaborator and problem-solver, recognized for improving performance and usability.
+                </p>
+
+                <a href="#projects" className="btn-primary">
+                    View My Projects
+                </a>
+
+            </div>
+
+            <div className="flex-1 flex justify-center mb-8 md:mb-0">
+
+                <img 
+                src={rudraPic} 
+                alt="Rudra Patel" 
+                className="w-48 h-48 md:w-64 md:h-64 rounded-full object-cover shadow-lg float" />
+
+            </div>
+
+        </section>
+    )
+}
